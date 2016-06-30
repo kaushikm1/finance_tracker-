d29968bbfa9ad6700473910028fdb430d5cd8954
Kaushik <kaushikm@andrew.cmu.edu>
2016-06-30 15:36:40 +0000

    Stocks listing

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,5 +1,5 @@
 class UserStocksController < ApplicationController
-  before_action :set_user_stock, only: [:show, :edit, :update, :destroy]
+  before_action :set_user_stock, only: [:show, :edit, :update]
 
   # GET /user_stocks
   # GET /user_stocks.json
@@ -42,7 +42,7 @@ class UserStocksController < ApplicationController
     end
     
     
-    @user_stock = UserStock.new(user_stock_params)
+    #@user_stock = UserStock.new(user_stock_params)
 
     respond_to do |format|
       if @user_stock.save
@@ -72,9 +72,10 @@ class UserStocksController < ApplicationController
   # DELETE /user_stocks/1
   # DELETE /user_stocks/1.json
   def destroy
+    @user_stock = current_user.user_stocks.where(stock_id: params[:id]).first
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to user_stocks_url, notice: 'User stock was successfully destroyed.' }
+      format.html { redirect_to my_portfolio_path, notice: 'Stock was removed from portfolio.' }
       format.json { head :no_content }
     end
   end
